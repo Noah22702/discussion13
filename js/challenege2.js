3e84cb9d98047ca75baa982a31f29109c0dc2435
Noah22702 <99115026+Noah22702@users.noreply.github.com>
2024-04-15 17:37:10 -0400

    2

---
+++ js/challenege2.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+    var text = document.getElementById("billing");
+    var result = text.value;
+     console.log(result);
+});
+
+document.querySelector('input').addEventListener("click", function(){
+    console.log();
+    if();
+    document.getElementById("useBilling").classList.toggle("hidden");
+});
